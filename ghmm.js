a1d9257744a61538e6a1ee0cdb3574bf15cfb074
Forest Finnigan <forestfinnigan@berkeley.edu>
2018-08-18 14:55:41 -0700

    Added propagate

--- ghmm.js
+++ ghmm.js
@@ -40,10 +40,61 @@ class GHMM {
 	propagate(Y, n){
 		this.alpha = math.zeros(n , this.n_hidden);
 		this.gamma = math.zeros(n, this.n_hidden);
-		this.zai = math.zeros(n, this.n_hidden, this.n_hidden);
-		this.Y = Y;
+		this.zai = math.zeros(this.n_hidden, this.n_hidden);
+		this.Y = Y;		
 
-		
-		window.alert(this.alpha);
+		this.alpha = math.subset(this.alpha, math.index(0, [0,1,2]), math.dotMultiply(this.pi, this.p(math.squeeze(math.subset(Y, math.index(0, [0, this.out_dim - 1]))))));
+		this.alpha = math.subset(this.alpha, math.index(0, [0,1,2]), math.divide(math.subset(this.alpha, math.index(0, [0,1,2])), math.sum(this.alpha)));
+		var i;
+		for(i = 1; i < n; i++){
+			this.alpha = math.subset(this.alpha, math.index(i, math.range(0,this.n_hidden)), math.dotMultiply(this.p(math.squeeze(math.subset(Y, math.index(i, math.range(0,this.out_dim))))), math.multiply(math.subset(this.alpha, math.index(0, math.range(0,this.n_hidden))), this.A)));
+			this.alpha = math.subset(this.alpha, math.index(i, math.range(0, this.n_hidden)), math.divide(math.subset(this.alpha, math.index(i, math.range(0, this.n_hidden))), math.sum(math.subset(this.alpha, math.index(i, math.range(0,this.n_hidden))))));
+		}
+		this.gamma = math.subset(this.gamma, math.index(n - 1, math.range(0, this.n_hidden)), math.subset(this.alpha, math.index(n - 1, math.range(0, this.n_hidden))));
+		var j;
+		for(j = n - 2; j > -1; j--){
+			this.gamma = math.subset(this.gamma, math.index(j, math.range(0, this.n_hidden)), math.dotDivide(math.dotMultiply(math.subset(this.alpha, math.index(j, math.range(0, this.n_hidden))),  math.multiply(math.subset(this.gamma, math.index(j+1, math.range(0, this.n_hidden))), math.transpose(this.A))), math.multiply(math.subset(this.alpha, math.index(j, math.range(0, this.n_hidden))), this.A)));	
+		}
+
+		var k;
+		for (k = 0; k < n - 1; k++){
+			this.zai = math.add(
+						 math.multiply(
+				         math.multiply(
+									   math.transpose(math.subset(this.alpha, math.index(k, math.range(0, this.n_hidden)))),
+									   math.dotDivide(
+									 			      math.dotMultiply(math.subset(this.gamma, math.index(k + 1, math.range(0, this.n_hidden))), this.p(math.squeeze(math.subset(this.Y, math.index(k + 1, math.range(0, this.out_dim)))))),
+									 				  math.add(math.subset(this.alpha, math.index(k + 1, math.range(0, this.n_hidden))), .0000000000000001))),
+				         			   this.A),
+						 this.zai);
+		}
+	}
+	evaluate(y){
+		this.old_alpha = math.dotMultiply(this.p(y), math.multiply(this.old_alpha, this.A));
+		this.old_alpha = math.divide(this.old_alpha, math.sum(this.old_alpha));
+		return this.old_alpha; 
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
