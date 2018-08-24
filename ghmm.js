9eb8390f555905bc135f8284430df1dc4b992b8c
Evan Finnigan <evanf@berkeley.edu>
2018-08-24 16:39:35 -0700

    made only current viewing position appear in plotter

--- ghmm.js
+++ ghmm.js
@@ -69,6 +69,68 @@ class GHMM {
 						 this.zai);
 		}
 	}
+
+	sum_columns(matrix){
+		var m = matrix._size[0];
+		var n = matrix._size[1];
+		var output = math.ones(1, n);
+
+		var i;
+		var curr_sum;
+		for(i = 0; i < n; i++){
+			curr_sum = math.sum(math.subset(matrix, math.index([0, m - 1], i)));
+			output = math.subset(output, math.index(0,i), curr_sum);
+		}
+
+		return math.squeeze(output);
+	}
+
+	update(n){
+		this.pi = math.subset(this.gamma, math.index(0, math.range(0, this.n_hidden)));
+
+		var i;
+		for(i = 0; i < this.n_hidden; i++){
+			var curr_gamma = math.squeeze(math.subset(this.gamma, math.index(math.range(0, n), i)));
+			var temp_numerator = math.multiply(math.transpose(curr_gamma), this.Y);
+			var temp_denominator = math.sum(curr_gamma);
+
+			this.mean[i] = math.divide(temp_numerator, temp_denominator);
+		}
+
+		for(i = 0; i < this.n_hidden; i++){
+			//this will help to subtract the mean from each element in each row
+			var temp_mean_matrix = math.multiply(math.ones(n, this.out_dim), math.diag(this.mean[i]));
+
+			var Y_shifted = math.subtract(this.Y, temp_mean_matrix);
+
+			var curr_gamma = math.squeeze(math.subset(this.gamma, math.index(math.range(0, n), i)));
+
+			var temp_numerator = math.multiply(math.transpose(Y_shifted), math.multiply(math.diag(curr_gamma), Y_shifted));
+			var temp_denominator = math.sum(curr_gamma);
+
+			this.cov[i] = math.divide(temp_numerator, temp_denominator);
+
+		}
+
+		var temp_sum_zai = this.zai;
+		var temp_sum_gamma = this.sum_columns(this.gamma);
+		this.A = math.dotDivide(temp_sum_zai, math.multiply(math.ones(this.n_hidden, this.n_hidden), math.diag(temp_sum_gamma)));
+
+		for(i = 0; i < this.A._size[1]; i++){
+			var temp_sum = math.sum(math.subset(this.A, math.index(math.range(0, this.A._size[0]), i)));
+			var temp_normalized_row = math.divide(math.subset(this.A, math.index(math.range(0, this.A._size[0]), i)), temp_sum);
+			this.A = math.subset(this.A, math.index(math.range(0, this.A._size[0]), i), temp_normalized_row);
+		}
+	}
+
+	train(Y){
+		var i;
+		for(i = 0; i < 100; i++){
+			this.propagate(Y, Y._size[0]);
+			this.update(Y._size[0]);
+		}
+	}
+
 	evaluate(y){
 		this.old_alpha = math.dotMultiply(this.p(y), math.multiply(this.old_alpha, this.A));
 		this.old_alpha = math.divide(this.old_alpha, math.sum(this.old_alpha));
