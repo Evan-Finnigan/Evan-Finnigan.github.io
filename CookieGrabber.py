5a4e243339ccd5b2b3077b4a3b25f28898ddf28b
Evan Finnigan <evanf@berkeley.edu>
2019-07-28 14:45:38 -0700

    added pages, and website style

--- CookieGrabber.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://evan-finnigan.github.io"
-r = requests.get(url,timeout=5)
-if r.status_code == 200:
-    for cookie in r.cookies:
-        print(cookie)            # Use "print cookie" if you use Python 2.
